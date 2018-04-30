983993bd0dd106d4977e4ad495250e71bed6f3f3
Joe Orr <joe.orr@gmail.com>
2018-04-30 06:26:38 -0400

    move functions to module

--- routes/rg.js
+++ routes/rg.js
@@ -1,102 +1,12 @@
-const assert = require('assert')
-const restify = require('restify-clients')
+const scraper = require('../lib/scraper')
+
 const nconf = require('nconf')
-const jsdom = require('jsdom');
-const _ = require('lodash')
-const { JSDOM } = jsdom
-const moment = require('moment')
 
 nconf.argv()
   .env()
   .file({ file: '.config.json' })
 
 const url = 'https://www.researchgate.net'
-
-/**
- *
- * @param selectors {object} Object with css selectors for plucking data
- * @param section {string} e.g. /profile/, the subpath in the target API for a particular endpoint
- * @param id {string} id for path, e.g. name of author
- * @param res {object} the restify response object
- */
-function returnScrapedJSON (selectors, section, id, res) {
-  const client = restify.createClient({url});
-  client.get(section + id, function (err, req) {
-    if (err) { console.log('Error:', err) }
-    req.on('result', function (err, tres) {
-      if (err) { console.log('Error result:', err) }
-      tres.body = ''
-      tres.setEncoding('utf8')
-      tres.on('data', function (chunk) {
-        tres.body += chunk
-      })
-      tres.on('end', function () {
-        const body = tres.body
-        const dom = new JSDOM(body)
-        const data = getDataFromSelectors(dom, selectors)
-        data.url = url + section + id
-        return res.json(data)
-      })
-    })
-  })
-}
-
-function getDomList (dom, selector) {
-  const items = []
-  let els = dom.querySelectorAll(selector.selector)
-  const subselectors = selector.subselectors
-  els.forEach(el => {
-    let item = {}
-    _.each(subselectors, (s) => {
-      let t = ''
-      let propName = Object.keys(s)[0]
-      let propSelector = s[propName]
-      if (el.querySelector(propSelector)) {
-        t = getDomText(el, propSelector)
-      }
-      item[propName] = t
-    })
-    items.push(item)
-  })
-  return items
-}
-
-function getDomText (dom, selector) {
-  let text = ''
-  if (_.isObject(selector)) {
-    return getDomList(dom, selector)
-  }
-  let el = dom.querySelector(selector)
-  if (el) {
-    text = el.textContent.replace(/\s+·\s+/,'').trim() // hack for garbage in rg text
-    if (/^meta/.test(selector)) {
-      console.log('meta')
-      text = el.getAttribute('content').trim()
-    }
-    if (el.tagName === 'A') {
-      text = {
-        href: el.getAttribute('href'),
-        text: text
-      }
-    }
-  }
-  return text
-}
-function getDataFromSelectors (dom, selectors) {
-  const data = {}
-  _.each(selectors, (v, k) => {
-    let t = getDomText(dom.window.document, v)
-    if ((k === 'date') && t) {
-      try {
-        t = moment(t).format('YYYY-MM')
-      } catch (e) {
-
-      }
-    }
-    data[k] = t
-  })
-  return data
-}
 
 module.exports = function (server) {
 
@@ -144,7 +54,7 @@ module.exports = function (server) {
         ]
       }
     }
-    returnScrapedJSON(selectors, '/publication/', id, res)
+    scraper.scrape(selectors, url, '/publication/', id, res)
   })
 
   /**
@@ -172,7 +82,7 @@ module.exports = function (server) {
    *           content-type: "text/plain"
    */
   server.get('/rg/author', function (req, res, next) {
-    var id = req.query.name
+    const id = req.query.name
     const selectors = {
       score: 'DIV[title="RG Score"]',
       title: 'SPAN.title',
@@ -183,7 +93,7 @@ module.exports = function (server) {
       reads: 'DIV.section-about>DIV:nth-child(2) .nova-o-grid.nova-o-grid--gutter-xxl.nova-o-grid--order-normal.nova-o-grid--horizontal-align-left.nova-o-grid--vertical-align-top DIV:nth-child(2)>DIV',
       citations: 'DIV.section-about>DIV:nth-child(2) .nova-o-grid.nova-o-grid--gutter-xxl.nova-o-grid--order-normal.nova-o-grid--horizontal-align-left.nova-o-grid--vertical-align-top DIV:nth-child(3)>DIV'
     }
-    returnScrapedJSON(selectors, '/profile/', id, res)
+    scraper.scrape(selectors, url, '/profile/', id, res)
   })
 
 }
